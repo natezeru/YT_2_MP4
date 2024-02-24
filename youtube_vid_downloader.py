3a51b5044b586df197595b046b0ee0ddc8220fa0
Nate Zeru <86619014+natezeru@users.noreply.github.com>
2024-02-24 09:45:00 +0700

    Add files via upload

---
+++ youtube_vid_downloader.py
@@ -0,0 +1,39 @@
+from pytube import YouTube
+import tkinter as tk
+from tkinter import filedialog
+
+
+def download_video(url, save_path):
+    try: 
+        yt = YouTube(url)
+        streams = yt.streams.filter(progressive=True, file_extension= "mp4")
+        highest_res = streams.get_highest_resolution()
+        highest_res.download(output_path=save_path)
+        print("Video download sucessfully")
+    except Exception as e:
+        print(e)
+
+
+download_video(url, save_path)
+
+
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+    if folder:
+        print(f"Selected folder: {folder}")
+
+    return folder
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.withdraw()
+    video_url = input("Please enter a yt url: ")
+    save_dir = open_file_dialog()
+    if not save_dir:
+        print("Invaild save location....")
+    else:
+        print("Started download...")
+        download_video(video_url,save_dir)
+
